19985cb1fe244dcd983bbc13709a92891c562686
Doni Rubiagatra <doni.rubiagatra@kumparan.com>
2018-03-18 09:34:12 +0700

    [playground] adding mongodb-delete

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,37 +1,35 @@
-/* const MongoClient = require('mongodb').MongoClient;
- */
+// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
- 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-    if (err) {
-        return console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected to MongoDB server');
-    const db = client.db('TodoApp')
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
- /*    db.collection('Todos').insertOne({
-        text: "Something to do",
-        completed: false
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert todo', err);
-        } 
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    }); */
-
-    /* db.collection('Users').insertOne({
-        name: "Andrew",
-        age: 25,
-        location: "Philadelphia"
-    }, (err, result) => {
-        if (err) {
-            return console.log("Unable to insert", err);
-        }
-    }); */
-
-    client.close();
+  db.close();
 });
-
